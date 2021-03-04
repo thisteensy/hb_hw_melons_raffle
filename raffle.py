9a2df46920f07ee8ef3cc34a65487a9c015f7a83
Tina Haiser <tinahaiser@gmail.com>
2021-03-04 03:39:27 +0000

    fixed import error in raffle.py

--- raffle.py
+++ raffle.py
@@ -42,7 +42,7 @@ def get_customers_from_file(customer_file_path):
 def pick_winner(customers):
     """Choose a random winner from list of customers."""
 
-    chosen_customer = random.choice(customers)
+    chosen_customer = choice(customers)
     
     name = chosen_customer.name
     email = chosen_customer.email
