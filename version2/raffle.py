bdd630f56c903f3110c03f4183f8677041e87172
Tina Haiser <tinahaiser@gmail.com>
2021-03-04 04:53:09 +0000

    Final FINAL commit. added dunder condition at the end of both raffle files.

--- version2/raffle.py
+++ version2/raffle.py
@@ -10,6 +10,8 @@ def choose_winner():
     winner = choice(get_customers_from_file("customers.txt"))
     print(f"Tell {winner.name} at {winner.email} that they have won the raffle.")
 
-choose_winner()
+
+if __name__ == "__main__"
+    choose_winner()
 # Hint: remember to import any functions you need from
 # other files or libraries
