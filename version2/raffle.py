2ed22e98f34b1f7358034972a13ccdcd7d5c915f
Tina Haiser <tinahaiser@gmail.com>
2021-03-04 04:08:43 +0000

    Completed raffle.py in version2. Everything work.

--- version2/raffle.py
+++ version2/raffle.py
@@ -2,5 +2,14 @@
 
 # Add code starting here
 
+from random import choice
+
+from customers import get_customers_from_file
+
+def choose_winner():
+    winner = choice(get_customers_from_file("customers.txt"))
+    print(f"Tell {winner.name} at {winner.email} that they have won the raffle.")
+
+choose_winner()
 # Hint: remember to import any functions you need from
 # other files or libraries
